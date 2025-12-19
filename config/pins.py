fd121601c1372d704f8f53c3ea9606287fcc355c
Nasuf Caliskan <nasufcaliskan@gmail.com>
2025-12-19 18:41:51 +0300

    İlk çalışan sürüm - motorlar ve GPIO tamam

---
+++ config/pins.py
@@ -0,0 +1,36 @@
+"""
+GPIO Pin Tanimlamalari
+
+"""
+
+# =========================
+# STEP MOTORLAR (DRV8825)
+# =========================
+
+# Motor 1
+MOTOR1_STEP = 18   # Hardware PWM
+MOTOR1_DIR  = 17
+
+# Motor 2
+MOTOR2_STEP = 19   # Hardware PWM
+MOTOR2_DIR  = 27
+
+
+# =========================
+# ULTRASONIC SENSOR (HC-SR04)
+# =========================
+TRIG = 16
+ECHO = 20
+
+
+# =========================
+# I2C (MPU9250)
+# =========================
+I2C_SDA = 2
+I2C_SCL = 3
+
+
+# =========================
+# DURUM LED
+# =========================
+STATUS_LED = 5
